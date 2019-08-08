42ff774a0d8ea062aa7e2a37e8a19271913630c9
tenda150 <alexeyprikazchik@gmail.com>
2019-08-08 17:11:44 +0300

    migrations

--- database/migrations/2019_08_08_110802_create_tasks_application_type.php
+++ database/migrations/2019_08_08_110802_create_tasks_application_type.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTasksApplicationType extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::dropIfExists('application_type');
+        
+        Schema::create('application_type', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('application_type_id');
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        
     }
 }
---
+++ database/migrations/2019_08_08_160717_create_tasks_table_group_people.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTableGroupPeople extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function down()
+    {
+        
+    }
+    public function up()
+    {
+        Schema::dropIfExists('group_people');
+        
+        Schema::create('group_people', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('group_people_id');
+            $table->integer('application_id')->unsigned();
+            $table->index('application_id');
+            $table->string('name', 50);
+            $table->integer('count_people');
+            $table->integer('average_age');
+            $table->integer('file');
+            $table->timestamps();
+        });
+        
+        Schema::table('group_people', function (Blueprint $table) {
+            
+            $table->foreign('application_id')->references('application_id')->on('application');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    
+}
