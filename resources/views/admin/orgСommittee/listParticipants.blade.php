3b3fc40ea4c3a66439bddcb612243a32e69a8b5d
misha1997 <mishaotroshenko2013@gmail.com>
2019-08-14 17:44:10 +0300

    list participants, evaluation results pages

--- resources/views/admin/orgСommittee/listParticipants.blade.php
+++ resources/views/admin/orgСommittee/listParticipants.blade.php
@@ -2,9 +2,28 @@
 
 @section('content')
 
-        <form role="form">
-              <input type="text" class="form-control">
-        </form>
-        <div>dsdds</div>
+      <form role="form">
+            <div style="margin-bottom: -35px;margin-left: 10px; font-size: 24px;"><i class="fa fa-search" aria-hidden="true"></i></div>
+            <input style="padding-left: 40px;" type="text" class="form-control">
+      </form>
+      <br>
+      <table class="table table-bordered">
+      <thead>
+            <tr>
+            <th>Example</th>
+            <th>Example</th>
+            <th>Example</th>
+            <th width="30px"></th>
+            </tr>
+      </thead>
+      <tbody>
+            <tr>
+            <td>Example</td>
+            <td>Example</td>
+            <td>Example</td>
+            <td><a href="#"><i class="fa fa-times" aria-hidden="true"></i></a></td>
+            </tr>
+      </tbody>
+      </table>
 
 @endsection
