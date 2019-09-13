962d2e634f4ed62e5a635d92b962f1cf323553a3
misha1997 <mishaotroshenko2013@gmail.com>
2019-09-13 12:40:47 +0300

    fix archive

--- resources/js/components/RemovedStatementComponent.vue
+++ resources/js/components/RemovedStatementComponent.vue
@@ -4,54 +4,37 @@
             <i class="fa fa-search" aria-hidden="true"></i>
             <input v-model="search" type="text" class="form-control">
         </form>
-        <br>
-        
+        <br> 
       <table class="table table-bordered accordion" id="accordion">
           <thead>
                 <tr>
-                    
-                    <th>#</th>
-                    <th>Example</th>
-                    <th>Example</th>
-                    
+                    <th>№</th>
+                    <th>ПІБ Учасника / Назва групи</th>
+                    <th>Тип заявки</th>
                     <th width="30px"></th>
                     <th width="30px"></th>
                 </tr>
           </thead>
           
-          <tbody class="card" v-for="(item, index) in filteredList">
+          <tbody class="card" v-for="(item, index) in filteredList" :key="index">
                 <tr>
                     <td data-toggle="collapse" :data-target="'#collapse'+(index+1)">
                         {{index+1}}
                     </td>
-                    <td data-toggle="collapse" :data-target="'#collapse'+(index+1)">
-                        {{ item.solo_duet ? item.solo_duet.name + ' ' + item.solo_duet.surname + ' ' + item.solo_duet.patronomic : item.group.name}}
-                    </td>
-                    <td data-toggle="collapse" :data-target="'#collapse'+(index+1)">
-                        {{ item.app_type.name }}
-                    </td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.type }}</td>
                     <td>
-                        <a href="#" @click="unarchiveMember(item.application_id)"><i class="fa fa-2x fa-check-circle" aria-hidden="true"></i></a>
-                                           
+                        <a href="#" @click="unarchiveMember(item.id)"><i class="fa fa-2x fa-check-circle" aria-hidden="true"></i></a>                
                     <td>
-
-                        <a href="#" @click="deleteMember(item.application_id)"><i class="fa fa-2x fa-trash" aria-hidden="true"></i></a>
+                        <a href="#" @click="deleteMember(item.id)"><i class="fa fa-2x fa-trash" aria-hidden="true"></i></a>
                     </td>
                 </tr>
                 <tr id="collapse1" class="collapse "  data-parent="#accordion">
                     <td colspan="5" class="card-body">
-                      
                         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                      
                     </td>
                 </tr>
             </tbody>
-
-            
-
-              
-          
-    
       </table>
     </div>
 </template>
@@ -60,8 +43,6 @@
     export default {
         data() {
             return {
-                keywords: null,
-                results: [],
                 members: [],
                 search: '',
             };
@@ -69,43 +50,45 @@
 
         created () {
             this.getFullList();
-
         },
         computed: {
             filteredList() {
                 return this.members.filter(members => {
-                    if(!members.group) {
-                        return members.solo_duet.name.toLowerCase().includes(this.search.toLowerCase())
-                    } else {
-                        return members.group.name.toLowerCase().includes(this.search.toLowerCase())
-                    }
+                    return members.name.toLowerCase().includes(this.search.toLowerCase()) || members.type.toLowerCase().includes(this.search.toLowerCase())
                 })
             }
         },
         methods: {
-
             getFullList(){
                 axios.get('/get-members')
-                    .then((response) => {
-
-                        this.members = response.data.filter(app =>{
-
-                            return app.status =="archive";
-                        });
-
-
-                    })
-                    .catch((error) => {
-                        swal({
-                            icon: "error",
-                            title: 'Помилка',
-                            text: error.response.status
-                        });
+                .then((response) => {
+                    this.members = [];
+                    response.data.forEach(member => {
+                        if(member.solo_duet.length == 0 && member.status == "archive") {
+                            this.members.push({
+                                name: member.group.name, 
+                                type: member.app_type.name,
+                                id: member.application_id
+                            })
+                        }
+                        else if(member.solo_duet.length == 1 && member.status == "archive") {
+                            this.members.push({
+                                name: `${member.solo_duet[0].name} ${member.solo_duet[0].surname} ${member.solo_duet[0].patronomic}`, 
+                                type: member.app_type.name,
+                                id: member.application_id
+                            })
+                        }
+                        else if(member.solo_duet.length == 2 && member.status == "archive") {
+                            this.members.push({
+                                name: `${member.solo_duet[0].name} ${member.solo_duet[0].surname} ${member.solo_duet[0].patronomic}, ${member.solo_duet[1].name} ${member.solo_duet[1].surname} ${member.solo_duet[1].patronomic}`, 
+                                type: member.app_type.name,
+                                id: member.application_id
+                            })
+                        }
                     });
+                })
             },
             unarchiveMember(id){
-
-
                 axios.post('/unarchive-members/'+id)
                     .then((response) => {
                         if(response.status == 200 ) {
@@ -125,8 +108,6 @@
                     });
             },
             deleteMember(id){
-
-
                 axios.post('/delete-members/'+id)
                     .then((response) => {
                         if(response.status == 200 ) {
