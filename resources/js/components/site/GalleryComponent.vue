cc8f80af37a81e6adb0cf8298db80108885a56ba
misha1997 <mishaotroshenko2013@gmail.com>
2019-09-20 10:49:25 +0300

    gymn page (back-end)

--- resources/js/components/site/GalleryComponent.vue
+++ resources/js/components/site/GalleryComponent.vue
@@ -6,31 +6,10 @@
             <div class="container">
                 <h2 class="title-section">ФЕСТИВАЛЬ ДЖАЗ-ВIтраж</h2>
                 <div class="row">
-                    <router-link :to="{ name: 'gallery-year', params:{ id: 2}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
+                    <router-link :to="{ name: 'gallery-year', params:{ id: 2}}" class="col-xl-4 gallery-item" v-for="item in foto" :key="item.foto_id">
+                        <img :src="'img/uploads/'+item.file" alt="" class="gallery-img">
+                        <div class="dark-bg"><p class="caption">{{ item.year }}</p></div>
                     </router-link>
-                    <router-link :to="{ name: 'gallery-year', params:{ id: 2}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
-                    </router-link>
-                    <router-link :to="{ name: 'gallery-year', params: { id: 3}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
-                    </router-link>
-                    <router-link :to="{ name: 'gallery-year', params: { id: 1}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
-                    </router-link>
-                    <router-link :to="{ name: 'gallery-year', params: { id: 1}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
-                    </router-link>
-                    <router-link :to="{ name: 'gallery-year', params: { id: 1}}" class="col-xl-4 gallery-item">
-                        <img src="img/gallery-img.png" alt="" class="gallery-img">
-                        <div class="dark-bg"><p class="caption">2015</p></div>
-                    </router-link>
-
                 </div>
 
 
@@ -47,20 +26,33 @@
     export default {
         data() {
             return {
-
+                foto: [],
             };
         },
 
         created () {
-
-
-        },
-        computed: {
-
+            this.getFoto();
         },
         methods: {
-
-
+            getFoto() {
+                axios.get('/get-foto')
+                .then((response) => {
+                    this.foto = response.data.filter(item => {
+                        return item.year
+                        // this.foto.filter((o, item) => {
+                        //     item.year != o.year
+                        // })
+                    })
+                    // response.data.map(item => {
+                    //     if (!this.foto.includes(item.year)) {
+                    //         this.foto.push({year: item.year, file: item.file})
+                    //     }
+                    // })
+                })
+                .then(() => {
+                    console.log(this.foto)
+                })
+            },
         },
 
     }
