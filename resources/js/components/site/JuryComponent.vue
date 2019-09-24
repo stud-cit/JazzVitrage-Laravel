771429e7be67200f2d9371eb55e98fc14d26515b
Andrey <gorand.one@gmail.com>
2019-09-24 14:50:21 +0300

    slider

--- resources/js/components/site/JuryComponent.vue
+++ resources/js/components/site/JuryComponent.vue
@@ -8,12 +8,13 @@
                         <h2 class="title">НАШЕ ЖУРІ</h2>
                         <p class="subtitle">У КОНКУРСІ</p>
                     </div>
-                </div>     
-                <div class="jury-list">
+                </div>
+                <carousel   :dots="false"  :nav="false" class="jury-list">
+                    <template slot="prev"><span class="prev"><i class="fa arrows fa-arrow-circle-left fa-3x" aria-hidden="true"></i></span></template>
                     <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 1}}">
                         <img src="img/list_jury/Intersection_4.png" alt="">
                         <div class="items-title"></div>
-                    </router-link>           
+                    </router-link>
                     <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 2}}">
                         <img src="img/list_jury/Intersection_5.png" alt="">
                         <div class="items-title"></div>
@@ -21,14 +22,33 @@
                     <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 3}}">
                         <img src="img/list_jury/Intersection_6.png" alt="">
                         <div class="items-title"></div>
-                    </router-link>      
-                </div>
+                    </router-link>
+                    <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 1}}">
+                        <img src="img/list_jury/Intersection_4.png" alt="">
+                        <div class="items-title"></div>
+                    </router-link>
+                    <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 2}}">
+                        <img src="img/list_jury/Intersection_5.png" alt="">
+                        <div class="items-title"></div>
+                    </router-link>
+                    <router-link class="jury-items" :to="{ name: 'jury-member', params:{ id: 3}}">
+                        <img src="img/list_jury/Intersection_6.png" alt="">
+                        <div class="items-title"></div>
+                    </router-link>
+                    <template slot="next" class="asdasd"><span class="next"><i class="fa arrows fa-arrow-circle-right fa-3x" aria-hidden="true"></i></span></template>
+
+                </carousel>
+                <!--<div class="jury-list">-->
+
+                <!--</div>-->
             </div>
         </section>
     </div>
 </template>
 
 <script>
+    import carousel from 'vue-owl-carousel';
+
     export default {
          data() {
             return {
@@ -47,6 +67,7 @@
 
 
         },
+        components: { carousel },
 
     }
 </script>
