039e0ba5f0ddf970211a4cfab4188cefd78e6eee
Andrey <gorand.one@gmail.com>
2019-10-02 14:02:34 +0300

    valid

--- resources/js/components/site/ApplicationComponent.vue
+++ resources/js/components/site/ApplicationComponent.vue
@@ -627,6 +627,7 @@
                 fileTitle: {
                     memberBirthdayFile: 'завантажити файл',
                     member2BirthdayFile: 'завантажити файл',
+                    groupBirthdayFile: 'завантажити файл',
                     idFile: 'завантажити файл',
                     idFile2: 'завантажити файл',
                     compositionVideo: 'завантажити файл',
