43f3d9d400f42ff7e677770e6f5cba836bd642cf
doratoles <46978407+doratoles@users.noreply.github.com>
2019-10-15 12:16:51 +0300

    complate task

--- resources/js/components/NewStatementComponent.vue
+++ resources/js/components/NewStatementComponent.vue
@@ -39,25 +39,31 @@
                 <tr :id="'collapse'+(index+1)" class="collapse" data-parent="#accordion">
                     <td colspan="5" class="card-body">
                         <div class="col-5">
-                            <p>ПІП: {{ item.name }}</p>
+                            <p v-if="item.type == 'соліст' || item.type == 'дует'"><strong>ПІП:</strong> {{ item.name }}</p>
+                            <p v-else><strong>Назва колективу:</strong> {{ item.name }}</p>
                         </div>
-                        <div class="col-2"></div>
-                        <div class="col-5">
-                            <p>Тип: {{ item.type }}</p>
+                        <div class="col-5 type-style">
+                            <p><strong>Тип:</strong> {{ item.type }}</p>
                         </div>
                         <div class="mt-2"></div>
                         <div class="col-5">
-                            <label for="memberDate" class="brtop">Дата народження</label>
-                            <p id="memberDate">{{ item.memberDate }}</p>
+                            <label for="memberDate" class="brtop" v-if="item.type == 'соліст' || item.type == 'дует'">Дата народження</label>
+                            <p id="memberDate" v-if="item.type == 'соліст' || item.type == 'дует'">{{ item.memberDate }}</p>
 
-                            <label for="memberIdCode" class="brtop">Ідентифікаційний номер учня</label>
-                            <p id="memberIdCode">{{ item.idCode }}</p>
+                            <label for="memberAverage" class="brtop" v-if="item.type == 'ансамбль' || item.type == 'Хор' || item.type == 'Оркестр'">Середній вік учасників</label>
+                            <p id="memberAverage" v-if="item.type == 'ансамбль' || item.type == 'Хор' || item.type == 'Оркестр'">{{ item.memberAverage }}</p>
+
+                            <label for="memberIdCode" class="brtop" v-if="item.type == 'соліст' || item.type == 'дует'">Ідентифікаційний номер учня</label>
+                            <p id="memberIdCode" v-if="item.type == 'соліст' || item.type == 'дует'">{{ item.idCode }}</p>
+
+                            <label for="teacherIdCode" class="brtop" v-if="item.type == 'ансамбль' || item.type == 'Хор' || item.type == 'Оркестр'">Ідентифікаційний номер викладача</label>
+                            <p id="teacherIdCode" v-if="item.type == 'ансамбль' || item.type == 'Хор' || item.type == 'Оркестр'">{{ item.teacherIdCode }}</p>
 
                             <label for="schoolName" class="brtop">Назва мистецького навчального закладу</label>
                             <p id="schoolName">{{ item.schoolName }}</p>
 
                             <label for="schoolContact" class="brtop">Адреса, телефон та e-mail навч. закладу</label>
-                            <p id="schoolContact">{{ `${item.schoolAddress} + "/" ${item.schoolPhone} + "/" ${item.schoolEmail}` }}</p>
+                            <p id="schoolContact">{{ `${item.schoolAddress} / ${item.schoolPhone} / ${item.schoolEmail}` }}</p>
 
                             <label for="teacherDate" class="brtop">ПІП викладача</label>
                             <p id="teacherDate">{{ `${item.teacherSurname} ${item.teacherName} ${item.teacherPatronymic}` }}</p>
@@ -68,16 +74,11 @@
                             <label for="concertDate" class="brtop">ПІП концертмейстра</label>
                             <p id="concertDate">{{ `${item.concertSurname} ${item.concertName} ${item.concertPatronymic}` }}</p>
 
-                            <label class="brtop">Програма та хронометраж кожного твору</label>
-                            <p>Перший твір</p>
-                            <p>Назва: {{ item.compositionName }}</p>
-                            <p>Автор: {{ item.compositionAuthor }}</p>
-                            <p>Хронометраж: {{ item.compositionTiming }}</p>
-
-                            <p>Другий твір</p>
-                            <p>Назва: {{ item.compositionName2 }}</p>
-                            <p>Автор: {{ item.compositionAuthor2 }}</p>
-                            <p>Хронометраж: {{ item.compositionTiming2 }}</p>
+                            <label class="brtop mb-3">Програма та хронометраж кожного твору</label>
+                            <p>Перший твір:</p>
+                            <p class="composition-style">Назва: {{ item.compositionName }} Автор: {{ item.compositionAuthor }} Хронометраж: {{ item.compositionTiming }}</p>
+                            <p>Другий твір:</p>
+                            <p class="composition-style">Назва: {{ item.compositionName2 }} Автор: {{ item.compositionAuthor2 }} Хронометраж: {{ item.compositionTiming2 }}</p>
                         </div>
                     </td>
                 </tr>
@@ -118,29 +119,29 @@ export default {
                 }).forEach(member => {
                     if(member.solo_duet.length == 0) {
                         this.members.push({
-                            surname: member.group.surname,
                             name: member.group.name,
-                            patronymic: member.group.patronymic,
                             type: member.app_type.name,
-	                        memberDate: member.group.memberDate,
-	                        idCode: member.group.idCode,
-	                        schoolName: member.group.schoolName,
-	                        schoolAddress: member.group.schoolAddress,
-	                        schoolPhone: member.group.schoolPhone,
-	                        schoolEmail: member.group.schoolEmail,
-	                        teacherSurname: member.group.teacherSurname,
-	                        teacherName: member.group.teacherName,
-	                        teacherPatronymic: member.group.teacherPatronymic,
-	                        teacherPhone: member.group.teacherPhone,
-	                        concertSurname: member.group.concertSurname,
-	                        concertName: member.group.concertName,
-	                        concertPatronymic: member.group.concertPatronymic,
-	                        compositionName: member.group.compositionName,
-	                        compositionAuthor: member.group.compositionAuthor,
-	                        compositionTiming: member.group.compositionTiming,
-	                        compositionName2: member.group.compositionName2,
-	                        compositionAuthor2: member.group.compositionAuthor2,
-	                        compositionTiming2: member.group.compositionTiming2,
+
+	                        memberAverage: member.group.average_age,
+	                        teacherIdCode: member.preparation.teacher_in,
+	                        schoolName: member.preparation.school_one,
+	                        schoolAddress: member.preparation.school_address,
+	                        schoolPhone: member.preparation.school_phone,
+	                        schoolEmail: member.preparation.school_email,
+	                        teacherSurname: member.preparation.teacher_surname,
+	                        teacherName: member.preparation.teacher_name,
+	                        teacherPatronymic: member.preparation.teacher_patronymic,
+	                        teacherPhone: member.preparation.teacher_phone,
+	                        concertSurname: member.preparation.concertmaster_surname,
+	                        concertName: member.preparation.concertmaster_name,
+	                        concertPatronymic: member.preparation.concertmaster_patronymic,
+	                        compositionName: member.presentation.composition_one,
+	                        compositionAuthor: member.presentation.author_one,
+	                        compositionTiming: member.presentation.time_one,
+	                        compositionName2: member.presentation.composition_two,
+	                        compositionAuthor2: member.presentation.author_two,
+	                        compositionTiming2: member.presentation.time_two,
+
                             id: member.application_id
                         })
                     }
@@ -148,13 +149,55 @@ export default {
                         this.members.push({
                             name: `${member.solo_duet[0].name} ${member.solo_duet[0].surname} ${member.solo_duet[0].patronymic}`,
                             type: member.app_type.name,
+
+	                        memberDate: member.solo_duet[0].data_birthday,
+	                        idCode: member.solo_duet[0].in,
+	                        schoolName: member.preparation.school_one,
+	                        schoolAddress: member.preparation.school_address,
+	                        schoolPhone: member.preparation.school_phone,
+	                        schoolEmail: member.preparation.school_email,
+	                        teacherSurname: member.preparation.teacher_surname,
+	                        teacherName: member.preparation.teacher_name,
+	                        teacherPatronymic: member.preparation.teacher_patronymic,
+	                        teacherPhone: member.preparation.teacher_phone,
+	                        concertSurname: member.preparation.concertmaster_surname,
+	                        concertName: member.preparation.concertmaster_name,
+	                        concertPatronymic: member.preparation.concertmaster_patronymic,
+	                        compositionName: member.presentation.composition_one,
+	                        compositionAuthor: member.presentation.author_one,
+	                        compositionTiming: member.presentation.time_one,
+	                        compositionName2: member.presentation.composition_two,
+	                        compositionAuthor2: member.presentation.author_two,
+	                        compositionTiming2: member.presentation.time_two,
+
                             id: member.application_id
                         })
                     }
                     else if(member.solo_duet.length == 2) {
                         this.members.push({
                             name: `${member.solo_duet[0].name} ${member.solo_duet[0].surname} ${member.solo_duet[0].patronymic}, ${member.solo_duet[1].name} ${member.solo_duet[1].surname} ${member.solo_duet[1].patronymic}`,
-                            type: member.app_type.name,
+	                        type: member.app_type.name,
+
+	                        memberDate: member.solo_duet[0].data_birthday,
+	                        idCode: member.solo_duet[0].in,
+	                        schoolName: member.preparation.school_one,
+	                        schoolAddress: member.preparation.school_address,
+	                        schoolPhone: member.preparation.school_phone,
+	                        schoolEmail: member.preparation.school_email,
+	                        teacherSurname: member.preparation.teacher_surname,
+	                        teacherName: member.preparation.teacher_name,
+	                        teacherPatronymic: member.preparation.teacher_patronymic,
+	                        teacherPhone: member.preparation.teacher_phone,
+	                        concertSurname: member.preparation.concertmaster_surname,
+	                        concertName: member.preparation.concertmaster_name,
+	                        concertPatronymic: member.preparation.concertmaster_patronymic,
+	                        compositionName: member.presentation.composition_one,
+	                        compositionAuthor: member.presentation.author_one,
+	                        compositionTiming: member.presentation.time_one,
+	                        compositionName2: member.presentation.composition_two,
+	                        compositionAuthor2: member.presentation.author_two,
+	                        compositionTiming2: member.presentation.time_two,
+
                             id: member.application_id
                         })
                     }
