e0c458b5e5575abcdc454df38365961bacd0d7d1
Andrey <gorand.one@gmail.com>
2019-10-18 10:48:23 +0300

    mail finish in contacts

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Question;
+use Mail;
 
 class QuestionController extends Controller
 {
@@ -18,6 +19,11 @@ class QuestionController extends Controller
         $question_data->name = $request->name;
         $question_data->email = $request->email;
         $question_data->question_text = $request->question_text;
+
+        Mail::send(['text' => 'mail'], ['name' => 'jazz'], function($message){
+           $message->to('gorand.two@gmail.com', 'test')->subject('заявка JazzVitrage');
+           $message->from('jazz@gmail.com', 'jazz');
+        });
 
         $question_data->save();
     }
