c825d2cd6ab148cc5935457d3cf6931853c9b509
Vadym <v.volin1488@gmail.com>
2019-12-11 11:59:15 +0200

    (Change : bg on header main page

--- resources/js/components/site/GalleryComponent.vue
+++ resources/js/components/site/GalleryComponent.vue
@@ -1,47 +1,48 @@
 <template>
-    <div>
-        <section class="sections main-section gallery">
-            <div class="container">
-                <h2 class="title-section">ФЕСТИВАЛЬ ДЖАЗ-ВIтраж</h2>
-                <div class="row">
-                    <router-link 
-                        class="col-12 col-md-6 col-xl-4 gallery-item"
-                        :to="{ name: 'gallery-year', params: { id: item.year } }" 
-                        v-for="item in foto" :key="item.foto_id"
-                    >
-                        <img :src="'img/uploads/'+item.file.file" class="gallery-img">
-                        <div class="dark-bg">
-                            <p class="caption">{{ item.year }}</p>
-                        </div>
-                    </router-link>
-                </div>
+  <div>
+    <section class="sections main-section gallery">
+      <div class="container">
+        <h2 class="title-section">ФЕСТИВАЛЬ ДЖАЗ-ВІТРАЖ</h2>
+        <div class="row">
+          <router-link
+            class="col-12 col-md-6 col-xl-4 gallery-item"
+            :to="{ name: 'gallery-year', params: { id: item.year } }"
+            v-for="item in foto"
+            :key="item.foto_id"
+          >
+            <img :src="'img/uploads/'+item.file.file" class="gallery-img" />
+            <div class="dark-bg">
+              <p class="caption">{{ item.year }}</p>
             </div>
-        </section>
-    </div>
+          </router-link>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                foto: []
-            };
-        },
-        created () {
-            this.getFoto();
-        },
-        methods: {
-            getFoto() {
-                axios.get('/get-foto')
-                .then((response) => {
-                    const years = [...new Set(response.data.map(item => item.year))];
-                    years.map(year => {
-                        this.foto.push({
-                            year, 
-                            file: response.data.filter(item => year == item.year)[0]})
-                    })
-                })
-            },
-        },
+export default {
+  data() {
+    return {
+      foto: []
+    };
+  },
+  created() {
+    this.getFoto();
+  },
+  methods: {
+    getFoto() {
+      axios.get("/get-foto").then(response => {
+        const years = [...new Set(response.data.map(item => item.year))];
+        years.map(year => {
+          this.foto.push({
+            year,
+            file: response.data.filter(item => year == item.year)[0]
+          });
+        });
+      });
     }
+  }
+};
 </script>
