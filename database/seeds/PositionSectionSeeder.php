2db3728a3c71ac9ed73ad98bf4f9ad459879430a
misha1997 <mishaotroshenko2013@gmail.com>
2020-02-07 16:07:36 +0200

    fix

--- database/seeds/PositionSectionSeeder.php
+++ database/seeds/PositionSectionSeeder.php
@@ -12,11 +12,12 @@ class PositionSectionSeeder extends Seeder
     public function run()
     {
         DB::table('position_section')->insert([
-            'provisions_text' => 'Протягом березня-квітня 2016 року в місті Суми проходив Другий обласний дитячий фестиваль-конкурс джазової музики "Джаз Вітраж". 
+            'provisions_text' => 'Протягом березня-квітня 2016 року в місті Суми проходив Другий обласний дитячий фестиваль-конкурс джазової музики "Джаз Вітраж".
 Головна мета цього нового творчого проекту - популяризація класичного та сучасного джазового мистецтва серед дітей та молоді',
             'video' => 'https://www.youtube.com/watch?v=fMKZMI8ByTs',
             'file' => '/file/file.docx',
             'personal_data' => '/file/personal.pdf',
+            'compress_url' => 'test'
         ]);
     }
 }
